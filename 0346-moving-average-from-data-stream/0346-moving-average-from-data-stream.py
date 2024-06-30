cd35b29f64fe1ea5b56def7c3d90bdba8909482f
sh kim <kim03208@naver.com>
2024-06-30 17:30:23 +0900

    Time: 88 ms (16.59%), Space: 19.6 MB (76.33%) - LeetHub

---
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.q = deque()
+        self.size = size
+        
+
+    def next(self, val: int) -> float:
+        if len(self.q) >= self.size: 
+            self.q.popleft()
+        
+        self.q.append(val)
+        return sum(self.q) / len(self.q)
