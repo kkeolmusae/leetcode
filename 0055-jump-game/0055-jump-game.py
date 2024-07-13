b3108c76f3a053b489a7be6d97d8ab63a7c42436
sh kim <kim03208@naver.com>
2024-07-13 12:10:43 +0900

    Time: 352 ms (62.72%), Space: 17.7 MB (91.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -3,9 +3,13 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        last_idx = len(nums) - 1
-        
-        for idx in range(len(nums) - 1, -1, -1):
-            if idx + nums[idx] >= last_idx:
-                last_idx = idx
-        return last_idx == 0
+        goal = len(nums) - 1  # 목적지 인덱스
+
+        # 제일 뒤에서 한칸 앞에서 부터 0번째까지 반복
+        for idx in range(goal - 1, -1, -1):
+            # idx 위치에서 최대 nums[idx] 만큼 이동했을떄 goal 이상 갈 수 있으면 도착 가능한거니깐
+            if idx + nums[idx] >= goal:
+                goal = idx
+        if goal == 0:
+            return True
+        return False
