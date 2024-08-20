bea1d65f6ec24cdd3d5eec6cd3db3211977595ff
sh kim <kim03208@naver.com>
2024-08-20 21:57:11 +0900

    Time: 264 ms (70.96%), Space: 25.7 MB (47.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,30 +1,25 @@
-from typing import List
-
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         total = 1
-        zero_count = 0
-        
+
+        # 0 개수가 2개이상이면 전체 배열이 0임.
+        is_zero_exist = False
+
         for num in nums:
             if num == 0:
-                zero_count += 1
-                if zero_count > 1:
-                    total = 0
+                if is_zero_exist:
+                    return [0] * len(nums)
+                is_zero_exist = True
             else:
                 total *= num
-        
+
         result = []
-        
-        if zero_count > 1:
-            return [0] * len(nums)
-        else:
-            for num in nums:
-                if num == 0:
-                    result.append(total)
-                elif zero_count > 0:
-                    result.append(0)
-                else:
-                    result.append(total // num)
-            
+        for num in nums:
+            if num == 0:  # 0이면 total 을 넣고
+                result.append(total)
+            elif is_zero_exist:  # 0이 아닌데 전체중에 0이 있으면 0을 넣고
+                result.append(0)
+            else:  # 0이 아예 없는 경우에는 total // num
+                result.append(total // num)
+
         return result
