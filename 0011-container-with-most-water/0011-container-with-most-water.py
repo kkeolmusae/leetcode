af2b9dd32384ab95f67d3f983fe5f2a097d7ba52
sh kim <kim03208@naver.com>
2024-08-21 22:58:09 +0900

    Time: 529 ms (37.26%), Space: 28.6 MB (99.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,15 +1,20 @@
+from typing import List
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-      result = 0
-      l_idx = 0
-      r_idx = len(height) - 1
-      
-      # 좌우에서 
-      while l_idx < r_idx:
-        result = max(result, (r_idx - l_idx) * min(height[l_idx], height[r_idx]))
-        if height[l_idx] < height[r_idx]:
-          l_idx += 1
-        else: 
-          r_idx -= 1
-      
-      return result
+        result = 0
+        l_idx = 0
+        r_idx = len(height) - 1
+
+        while l_idx < r_idx:
+            w = r_idx - l_idx
+            h = min(height[l_idx], height[r_idx])
+            result = max(result, w * h)
+
+            if height[l_idx] < height[r_idx]:  # 왼쪽이 짧으면 왼쪽꺼 이동해보기
+                l_idx += 1
+            else:  # 아니면 오른쪽꺼 이동
+                r_idx -= 1
+
+        return result
