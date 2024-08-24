c76e73d29b593ac989dd975fbc62053d6edc02c1
sh kim <kim03208@naver.com>
2024-08-24 17:42:05 +0900

    Time: 457 ms (51.9%), Space: 20.2 MB (15.67%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,7 +1,3 @@
-from collections import deque
-from typing import List
-
-
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         left = 0
@@ -12,12 +8,12 @@ class Solution:
         for idx in range(len(nums)):
 
             num = nums[idx]
-            if num != 1:
-                if zero_idx >= 0:  # 0을 여러번 만나는 경우
+            if num != 1:  # 0을 만나는 경우
+                if zero_idx >= 0:  # 0을 여러번 만나는 경우, left 갱신
                     left = zero_idx + 1
                 zero_idx = idx
             right += 1
 
-            result = max(result, (right - left) - 1)
+            result = max(result, (right - left) - 1)  # 하나는 무조건 삭제를 해야하니깐 -1
 
         return result
