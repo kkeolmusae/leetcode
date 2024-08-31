9ca1803168cf37a629205749354e8a189a2b9606
sh kim <kim03208@naver.com>
2024-08-31 19:11:47 +0900

    Time: 69 ms (38.32%), Space: 16.9 MB (85.62%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -9,7 +9,7 @@ class Solution:
         while q:
             key = q.pop()  # 내가 보유한 열쇠
             if visited_room[key] == 0:  # 방문한 적 없으면
-                q.extend(rooms[key])
+                q.extend(rooms[key])  # 해당 방에 있는 열쇠 다 q에 넣기
                 visited_room[key] = 1  # 방문처리
         if sum(visited_room) == len(rooms):
             return True
