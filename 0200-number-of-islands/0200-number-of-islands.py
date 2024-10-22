64c0f71336f8c95e9a4fa6f37c6e91c061666b88
sh kim <kim03208@naver.com>
2024-10-22 22:27:54 +0900

    Time: 245 ms (56.61%), Space: 18.8 MB (97.35%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        q = deque()  # 땅 위치
+        cnt = 0
+
+        garo = len(grid[0])
+        sero = len(grid)
+
+        # 오 아 왼 위 방향으로 회전
+        dxdy = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        for i in range(sero):
+            for j in range(garo):
+                if grid[i][j] == "1":
+                    q.append((i, j))
+                    cnt += 1
+                    while q:
+                        x, y = q.popleft()
+                        if grid[x][y] == "1":  # 땅인 경우에만
+                            grid[x][y] = "2"  # 확인처리
+                            for idx in range(4):
+                                nx = x + dxdy[idx][0]
+                                ny = y + dxdy[idx][1]
+
+                                if nx >= 0 and nx < sero and ny >= 0 and ny < garo:
+                                    if grid[nx][ny] == "1":
+                                        q.append((nx, ny))
+        return cnt
