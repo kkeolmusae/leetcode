0701823b30199ee7ea79fe165e6c9846d29457dc
sh kim <kim03208@naver.com>
2024-10-22 23:11:35 +0900

    Time: 0 ms (100%), Space: 16.7 MB (6.68%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            # (i-1 에서 올라오는 경우) + (i-2 에서 올라오는 경우)
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
