28f349a0cc4ab3327ec45b07abdd2dce29225afa
sh kim <kim03208@naver.com>
2024-11-30 16:37:47 +0900

    Time: 49 ms (72.76%), Space: 17.2 MB (5.42%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,18 +1,25 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        idx = 1
-        count = 1
+        MAX_NUM = math.inf
         n = len(nums)
+        if n < 3:
+            return n
+        cnt = 0
 
-        while idx < n:
-            if nums[idx] == nums[idx - 1]:
-                count += 1
+        num_idx = []
 
-                if count > 2:
-                    del nums[idx]
-                    idx -= 1
-                    n -= 1
-            else:
-                count = 1
-            idx += 1
-        return len(nums)
+        for i in range(2, n):
+            if nums[i] == nums[i - 1] and nums[i] == nums[i - 2]:
+                num_idx.append(i)  # 중복되는 수 위치 저장
+                cnt += 1
+
+        for idx in num_idx:  # 중복되는 수를 MAX_NUM으로 치환
+            nums[idx] = MAX_NUM
+
+        nums.sort()  # 오름차순 정렬
+
+        for idx in range(n):  # MAX_NUM -> "_" 으로 치환
+            if nums[idx] == MAX_NUM:
+                nums[idx] = "_"
+
+        return n - cnt
