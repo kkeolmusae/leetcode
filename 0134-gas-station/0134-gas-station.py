e61ea28a4ec20acd98f9924f695836cee326294c
sh kim <kim03208@naver.com>
2024-12-05 19:54:36 +0900

    Time: 32 ms (39.07%), Space: 22.9 MB (16.76%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,20 +1,19 @@
-from typing import List
-
-
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        total_gas = 0
-        curr_gas = 0
+        total = 0
+        curr = 0
         result = 0
 
         for idx in range(len(gas)):
-            total_gas += gas[idx] - cost[idx]
-            curr_gas += gas[idx] - cost[idx]
+            used_gas = gas[idx] - cost[idx]
+            total += used_gas
+            curr += used_gas
 
-            if curr_gas < 0:
-                curr_gas = 0
-                result = idx + 1
+            # curr 이 음수면 다음 지역으로 못간다는거임
+            if curr < 0:
+                curr = 0  # 초기화하고
+                result = idx + 1  # 현재 위치에서는 안되는거니깐 다음 위치로 설정
 
-        if total_gas >= 0:
+        if total >= 0:  # total이 양수면 모든 지역 다 갈 수 있다는거임
             return result
         return -1
