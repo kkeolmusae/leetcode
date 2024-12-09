12a87ec7e1e1edffa9b5fd6175a7722a0b37f628
sh kim <kim03208@naver.com>
2024-12-10 00:09:45 +0900

    Time: 23 ms (94.4%), Space: 26.5 MB (23.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,13 @@
-import math
-from typing import List
-
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = math.inf
-        max_price = 0
+        max_profit = 0  # 최대 수익
+        min_price = math.inf  # 최소 값
 
-        for idx in range(len(prices)):
-            if prices[idx] < min_price:
-                min_price = prices[idx]
-            elif prices[idx] - min_price > max_price:
-                max_price = prices[idx] - min_price
-
-        return max_price
+        for p in prices:
+            if p < min_price:
+                # min_price 보다 더 작은게 나왔으면 min 갱신
+                min_price = p
+            elif p - min_price > max_profit:
+                # 현재 가격 - min_price 가 max_profit 보다 크면, max_profit 갱신
+                max_profit = p - min_price
+        return max_profit
