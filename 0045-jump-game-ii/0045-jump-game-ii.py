3de99ddb94e3b64e6ab81037aba56d05292ba947
sh kim <kim03208@naver.com>
2024-12-11 00:52:53 +0900

    Time: 12 ms (37.41%), Space: 18.4 MB (10.43%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,18 +1,21 @@
-import math
-from typing import List
-
-
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        l = len(nums)
-
-        if l == 1:
+        if len(nums) <= 1 or nums[0] == 0:
             return 0
-        dp = [math.inf] * l
-        for i in range(l):
-            for j in range(i + 1, min(i + 1 + nums[i], l)):
-                if dp[i] == math.inf:
-                    dp[j] = 1
-                else:
-                    dp[j] = min(dp[i] + 1, dp[j])
-        return dp[l - 1]
+
+        curr = 0  # 현재 위치
+        can_jump = 0  # 현재 위치에서 점프 가능한 최대 위치
+        can_jump_max = 0  # 최대 점프 가능한 위치
+        last_idx = len(nums) - 1  # 마지막 위치
+        answer = 0
+
+        while curr < last_idx:
+            # 최대 점프 가능한 위치는 지속적으로 갱신
+            can_jump_max = max(curr + nums[curr], can_jump_max)
+
+            # 현재 위치에서 점프 가능한 최대 위치에 도달하면
+            if curr == can_jump:
+                answer += 1  # 점프 횟수 증가
+                can_jump = can_jump_max  # 현재 위치에서 점프 가능한 최대 위치 갱신
+            curr += 1  # 다음 발판으로
+        return answer
