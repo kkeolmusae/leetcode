d49c78ed3bbb475e5bd53c671ba369238541a0bf
sh kim <kim03208@naver.com>
2024-12-15 21:40:16 +0900

    Time: 17 ms (25.69%), Space: 17.6 MB (16.61%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,24 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        l = defaultdict(list)
+        lidx = 0
+        go_down = True
+
+        if numRows == 1:
+            return s
+
+        for idx in range(len(s)):
+            l[lidx].append(s[idx])  # 문자 넣기
+            lidx = lidx + 1 if go_down else lidx - 1
+
+            if lidx >= numRows or lidx < 0:  # 끝에 다달았으면
+                lidx = lidx + 2 if not go_down else lidx - 2
+                if go_down == True:
+                    go_down = False
+                else:
+                    go_down = True
+
+        result = ""
+        for idx in range(numRows):
+            result += "".join(l[idx])
+        return result
