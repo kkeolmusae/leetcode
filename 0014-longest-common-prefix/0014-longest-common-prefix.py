4a62bd4b0111b00543e3ad9d96b4c3e0af671eef
sh kim <kim03208@naver.com>
2024-12-15 20:39:30 +0900

    Time: 0 ms (100%), Space: 17.4 MB (9.27%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,27 +1,23 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-      if len(strs) == 1:
-        return strs[0]
+        n = len(strs)
+        if n == 1:
+            return strs[0]
 
-      result = ""
-      flag = True
-      idx = 0
+        same = 0
+        for idx in range(min(len(strs[0]), len(strs[1]))):
+            if strs[0][same] == strs[1][same]:
+                same += 1
+            else:
+                break
 
-      while flag:
-        if len(strs[0]) == 0 or idx >= len(strs[0]): # 첫번째 문자의 길이가 0 이거나, 첫번째 문자의 길이보다 idx가 커지면 끝
-          break
-
-        temp = strs[0][idx]
-
-        for i in range(1, len(strs)): # 두번째 문자부터 쭉 처리
-          str = strs[i]
-          
-          if len(str) <= idx or temp != str[idx]: # n번째 문자의 길이가 idx보다 작거나 비교하고자 하는 문자가 다르면 끝
-            flag = False
-            break
-        
-        if flag:
-          result += temp
-          idx += 1
-        
-      return result
+        for idx in range(1, n - 1):
+            if strs[idx][:same] != strs[idx + 1][:same]:
+                cnt = 0
+                for iidx in range(min(len(strs[idx]), len(strs[idx + 1]))):
+                    if strs[idx][iidx] == strs[idx + 1][iidx]:
+                        cnt += 1
+                    else:
+                        break
+                same = cnt
+        return strs[0][:same]
