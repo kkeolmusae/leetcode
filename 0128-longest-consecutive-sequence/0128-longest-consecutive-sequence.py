9c94915a5f9917fa2dd8682a3e45fa73f78fb493
sh kim <kim03208@naver.com>
2024-12-21 04:56:59 +0900

    Time: 48 ms (66.61%), Space: 33.4 MB (61.24%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,25 +1,18 @@
-from typing import List
-
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if len(nums) < 2:
-            return len(nums)
-        nums.sort()
-        
-        max_longest_consectuive = 0
-        
-        prev_num = nums[0]
-        cnt = 1
-        for idx in range(1, len(nums)):
-            if nums[idx] - prev_num == 1:
-                cnt += 1
-            elif nums[idx] == prev_num:
-                continue
-            else:
-                max_longest_consectuive = max(cnt, max_longest_consectuive)
-                cnt = 1
-            prev_num = nums[idx]
+        nums_set = set(nums)  # 모든 숫자 set에 넣어두고
+        result = 0
 
-        max_longest_consectuive = max(cnt, max_longest_consectuive)
-        return max_longest_consectuive  
+        for num in nums_set:  # 하나씩 꺼냄
+            # 꺼낸 숫자보다 작은 숫자가 없으면 (이 숫자가 시작숫자라는 뜻)
+            if num - 1 not in nums_set:
+
+                tmp = 1  # 초기값
+                curr_num = num  # 시작값
+                while curr_num + 1 in nums_set:  # 다음 숫자가 있는지 체크해서 있으면
+                    tmp += 1  # tmp += 1
+                    curr_num += 1  # 다음 값으로 넘어감
+
+                # 연속 숫자가 끊길때까지 하고 result 갱신
+                result = max(tmp, result)
+        return result
