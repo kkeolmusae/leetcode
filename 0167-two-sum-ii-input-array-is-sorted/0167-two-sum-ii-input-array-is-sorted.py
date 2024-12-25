35f135013844f655704df8c821aadc0c02aa831c
sh kim <kim03208@naver.com>
2024-12-26 01:21:26 +0900

    Time: 3 ms (81.63%), Space: 18.5 MB (9.49%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,20 +1,18 @@
-from typing import List
-
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         lidx = 0
         ridx = len(numbers) - 1
 
-        while lidx < ridx:
-            left = numbers[lidx]
-            right = numbers[ridx]
-            sums = left + right
+        while True:
+            x = numbers[lidx]
+            y = numbers[ridx]
 
-            if target == sums:
+            if x + y == target:  # 숫자가 target이랑 같으면 끝
                 return [lidx + 1, ridx + 1]
 
-            if sums > target:
+            if x + y > target:
+                # 숫자 합친게 target 보다 크면 오른쪽 인덱스를 왼쪽으로 이동
                 ridx -= 1
-            elif sums < target:
+            else:
+                # 숫자 합친게 target 보다 작으면 왼쪽 인덱스를 오른쪽으로 이동
                 lidx += 1
