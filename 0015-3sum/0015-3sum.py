e9e99f5b0d593d3a19da5bb74f540c19f9761ea7
sh kim <kim03208@naver.com>
2024-12-28 15:13:57 +0900

    Time: 1877 ms (6.49%), Space: 22.1 MB (5.05%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,37 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-      dic = {}
-      nums.sort()
-      
-      for num in nums:
-        if num not in dic: 
-          dic[num] = 1
-        else:
-          dic[num] += 1
-      
-      s = set()
-      result = []
-      
-      for i in dic:
-        for j in dic:
-          pick = (i + j) * -1
-          if pick in dic: # 두 값을 더한 것에서 x -1 한 값이 있으면
-            if i == j and j == pick and dic[i] < 3: # 다 같은데 개수가 안맞는 경우
-              continue
-            
-            if i == j and j != pick and dic[i] < 2:
-              continue
-            
-            if j == pick and i != j and dic[j] < 2:
-              continue
-            
-            if i == pick and i != j and dic[i] < 2:
-              continue
-            
-            sorted_arr = sorted([i, j, pick])
-            if tuple(sorted_arr) not in s:
-              result.append(sorted_arr)
-              s.add(tuple(sorted_arr))
+        nums.sort()
 
-        
-      return result
+        # 가장 작은 숫자가 0 보다 크면 0을 만들 수 없기에 빈 배열 리턴
+        if nums[0] > 0:
+            return []
+
+        dic = defaultdict(int)
+        for n in nums:
+            dic[n] += 1
+
+        if len(dic) == 1 and dic[0] >= 3:
+            return [[0, 0, 0]]
+
+        s = set()
+        result = []
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                a, b = nums[i], nums[j]
+                c = 0 - (nums[i] + nums[j])
+
+                if c not in dic:
+                    continue
+
+                if a == b and b == c and dic[c] < 3:
+                    continue
+
+                if a == b and b != c and dic[c] < 2:
+                    continue
+
+                if a == c and a != b and dic[a] < 2:
+                    continue
+
+                if b == c and a != b and dic[b] < 2:
+                    continue
+
+                tmp = sorted([a, b, c])  # 정렬해서 중복 제거
+
+                if tuple(tmp) not in s:
+                    result.append(tmp)
+                    s.add(tuple(tmp))
+
+        return result
