db2230964ef9aa4a0942cee8a710651a82d2ece8
sh kim <kim03208@naver.com>
2024-12-30 01:09:36 +0900

    Time: 19 ms (54.99%), Space: 17.9 MB (9.64%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,16 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-      max_length = 0
-      prev_idx = -1
-      key_map = {}
+        n = len(s)
+        if n == 0:
+            return 0
 
-      for idx in range(len(s)):
-        st = s[idx]# 현재 문자
-        
-        if st in key_map and key_map[st] > prev_idx: # 과거에 처리한 문자면, 마지막으로 이 문자를 발견한 위치를 기점으로 현재까지 위치 계산
-          prev_idx = key_map[st]
-        key_map[st] = idx
-        
-        max_length = max(idx - prev_idx, max_length)
+        idx = {}
+        st = ""
+        result = 0
+        start = 0
 
-      return max_length
+        for i in range(len(s)):  # 인덱스 기준으로 탐색
+            curr_char = s[i]  # 현재 문자
+            # 이미 등장한 문자이고, start 보다 뒤에 있을 때
+            if curr_char in idx and idx[curr_char] >= start:
+                # idx[curr_char]: 현재 문자의 이전 인덱스
+                st = s[idx[curr_char] + 1 : i]  # start ~ 현재 문자까지 슬라이싱
+                start = idx[s[i]] + 1  # start 갱신
+            st += s[i]  # st 에 현재 문자 추가
+            idx[s[i]] = i  # 현재 문자의 인덱스 저장
+            result = max(result, len(st))  # 최대 길이 갱신
+
+        return result
