9866c5e322cccdf40d3c77a341c5365b6071e965
sh kim <kim03208@naver.com>
2024-12-30 23:14:01 +0900

    Time: 0 ms (100%), Space: 18.3 MB (23.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,16 +1,16 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        garo = len(matrix[0])
-        sero = len(matrix)
+        width = len(matrix[0])
+        height = len(matrix)
 
-        zero = set()  # 0 위치 저장
-        for i in range(sero):
-            for j in range(garo):
+        zero = set()
+        for i in range(height):
+            for j in range(width):
                 if matrix[i][j] == 0:
                     zero.add((i, j))
 
         for i, j in zero:
-            for x in range(garo):  # 가로줄 0처리
-                matrix[i][x] = 0
-            for x in range(sero):  # 세로줄 0처리
-                matrix[x][j] = 0
+            for idx in range(height):
+                matrix[idx][j] = 0
+            for idx in range(width):
+                matrix[i][idx] = 0
