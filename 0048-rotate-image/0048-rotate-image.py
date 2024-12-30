e55959fd6bbb78678bc1f20def7963efb60260e9
sh kim <kim03208@naver.com>
2024-12-30 23:00:58 +0900

    Time: 0 ms (100%), Space: 17.8 MB (9.12%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -2,9 +2,11 @@ class Solution:
 
     def rotate(self, matrix: List[List[int]]) -> None:
 
-        n = len(matrix)  # 행 길이 계산
-        m = len(matrix[0])  # 열 길이 계산
-        temp = copy.deepcopy(matrix)
-        for i in range(n):
-            for j in range(m):
-                matrix[j][n - i - 1] = temp[i][j]
+        n = len(matrix)  # 길이 계산
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[j][n - i - 1]
+                matrix[j][n - i - 1] = matrix[i][j]
+                matrix[i][j] = matrix[n - j - 1][i]
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+                matrix[n - i - 1][n - j - 1] = tmp
