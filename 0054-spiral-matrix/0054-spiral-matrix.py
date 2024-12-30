8f00c7e2cdb7724afb2d70538f18bf1173d77eee
sh kim <kim03208@naver.com>
2024-12-30 19:20:33 +0900

    Time: 0 ms (100%), Space: 17.7 MB (12.12%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,23 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         result = []
-        check = set()
-        n = len(matrix[0])  # 가로
-        m = len(matrix)  # 세로
+        checked = math.inf
+        height = len(matrix)  # 세로
+        width = len(matrix[0])  # 가로
 
-        total = n * m  # 전체 숫자 개수
+        total = height * width  # 전체 숫자 개수
         x, y = 0, 0
 
-        direction = 0  # 방향 (처음에는 dxdy[0] 오른쪽)
+        direction = 0  # 방향
         dxdy = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 오 아 왼 위 방향으로 회전
 
         while total:
             result.append(matrix[x][y])  # 숫자 저장
-            total -= 1
-            check.add((x, y))
-            nx, ny = x + dxdy[direction % 4][0], y + dxdy[direction % 4][1]
-            if nx >= m or nx < 0 or ny < 0 or ny >= n or (nx, ny) in check:
-                direction += 1
-                nx, ny = x + dxdy[direction % 4][0], y + dxdy[direction % 4][1]
+            matrix[x][y] = checked  # 방문 처리
+            total -= 1  # 전체 개수 감소
+            nx, ny = x + dxdy[direction][0], y + dxdy[direction][1]  # 다음 좌표
+
+            # 회전해야하는 경우
+            if (
+                nx >= height
+                or nx < 0
+                or ny < 0
+                or ny >= width
+                or matrix[nx][ny] == checked
+            ):
+                direction = (direction + 1) % 4
+                nx, ny = x + dxdy[direction][0], y + dxdy[direction][1]
             x, y = nx, ny
         return result
