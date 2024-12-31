c9aa4c31f37d7be69592dfd2c378159f991a7b05
sh kim <kim03208@naver.com>
2024-12-31 16:46:51 +0900

    Time: 11 ms (89.04%), Space: 17.9 MB (11.13%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in set(ransomNote):
-            if ransomNote.count(i) > magazine.count(i):
+        ransom_counter = Counter(ransomNote)
+        magazine_counter = Counter(magazine)
+
+        for char, count in ransom_counter.items():
+            if magazine_counter[char] < count:
                 return False
         return True
