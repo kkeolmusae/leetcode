56c3144b644464bdc8363715189d64c6ee847f61
sh kim <kim03208@naver.com>
2024-12-31 20:10:06 +0900

    Time: 11 ms (81.64%), Space: 20.9 MB (40.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,14 @@
-from collections import defaultdict
-from typing import List
-
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = defaultdict(list)
-        
-        for str in strs:
-            sorted_str = sorted(str)
-            dic["".join(sorted_str)].append(str)
-        return list(dic.values())
+        tmp = []
+        for idx in range(len(strs)):  # 정렬한 문자 추가
+            tmp.append("".join(sorted(strs[idx])))
+
+        anagrams = defaultdict(list)
+        for idx in range(len(tmp)):  # 정렬한 문자를 key로 하여 그룹화
+            anagrams[tmp[idx]].append(strs[idx])
+
+        result = []
+        for key in anagrams:  # 그룹화된 문자들을 리스트로 변환
+            result.append(anagrams[key])
+        return result
