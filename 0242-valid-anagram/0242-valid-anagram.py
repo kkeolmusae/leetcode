e44d904822e70b5e4de70d04c583c055b8bf8a42
sh kim <kim03208@naver.com>
2024-12-31 17:16:41 +0900

    Time: 7 ms (90.81%), Space: 18 MB (11.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,12 @@
-from collections import defaultdict
-
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
-        dic = defaultdict(int)
-        for idx in range(len(s)):
-            dic[s[idx]] += 1
-            dic[t[idx]] -= 1
         
-        for d in dic:
-            if dic[d] != 0:
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+
+        for char, count in counter_s.items():
+            if counter_t[char] != count:
                 return False
         return True
