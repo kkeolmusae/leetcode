67b2d91fbb2aea0c684aac0416ee41bd0f32b139
sh kim <kim03208@naver.com>
2025-01-01 18:44:25 +0900

    Time: 3 ms (92%), Space: 21.3 MB (33.03%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,18 @@
-from collections import defaultdict
-import heapq
-from typing import List
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dict = defaultdict(int)
-        for num in nums: # O(n)
-            dict[num] += 1
-        
-        hq = []
-        for key in dict: # O(mlogm)
-            heapq.heappush(hq, (-dict[key], key))
-        
-        result = []
-        for _ in range(k): # O(klogm)
-            result.append(heapq.heappop(hq)[1])
 
+        for n in nums:
+            dict[n] += 1
+
+        q = []
+        for key, val in dict.items():
+            # 빈도가 많은게 우선순위가 높게 하기 위해서 - 붙임
+            heapq.heappush(q, (-val, key))
+
+        result = []
+        while k:
+            val, key = heapq.heappop(q)
+            result.append(key)
+            k -= 1
         return result
