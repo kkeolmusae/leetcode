b207fcd51eacd6afddba4181f9fe4692b7296d83
sh kim <kim03208@naver.com>
2025-01-01 18:37:47 +0900

    Time: 79 ms (21.83%), Space: 18.4 MB (6.84%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,14 +1,10 @@
+import base64
+
 class Codec:
-    def encode(self, strs):
-        """Encodes a list of strings to a single string."""
-        return '℘'.join(strs)
-        
-    def decode(self, s):
-        """Decodes a single string to a list of strings."""
-        return s.split('℘')
-        
+    def encode(self, strs: List[str]) -> str:
+        new_strs = "à".join(strs)  # 문자 사이에 non-ascii(à) 을 넣고
+        return base64.b64encode(new_strs.encode("UTF-8")).decode("ascii")  # 인코딩
 
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+    def decode(self, s: str) -> List[str]:
+        new_strs = base64.b64decode(s.encode("ascii")).decode("UTF-8")
+        return new_strs.split("à")
