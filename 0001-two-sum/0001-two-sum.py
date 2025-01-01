6c7d1f6af3d5cd73e7c0d674b0532974ac81ea36
sh kim <kim03208@naver.com>
2025-01-01 16:29:41 +0900

    Time: 0 ms (100%), Space: 18.8 MB (13.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+
+        for idx in range(len(nums)):
+            num = nums[idx]
+            if target - num in hash_map:
+                return [hash_map[target - num], idx]
+            hash_map[num] = idx
+        return []
