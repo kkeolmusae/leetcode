aae9d0c6cb47393d65db256dc67cb76713f63044
sh kim <kim03208@naver.com>
2025-01-12 19:24:31 +0900

    Time: 0 ms (100%), Space: 17.8 MB (39.13%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,16 @@
-from itertools import combinations
-from typing import List
-
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        result = []
-        for i in range(n + 1):
-            l = list(combinations(nums, i))
-            for li in l:
-                result.append(list(li))
+        answer = []
 
-        return result
+        def backtracking(curr: List[int], idx: int):
+            # 현재 상태를 결과에 추가 (깊은 복사 사용)
+            answer.append(curr[:])
+
+            # idx부터 시작해서 모든 원소를 탐색
+            for i in range(idx, len(nums)):
+                curr.append(nums[i])  # 현재 원소를 추가
+                backtracking(curr, i + 1)  # 다음 단계로 재귀 호출
+                curr.pop()  # 백트래킹: 상태 복원
+
+        backtracking([], 0)
+        return answer
