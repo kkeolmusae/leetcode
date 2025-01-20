a88527b3dfa075918996b771cba8193d0b86497a
sh kim <kim03208@naver.com>
2025-01-21 00:17:18 +0900

    Time: 10 ms (70.33%), Space: 27.7 MB (23.12%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,30 +1,29 @@
-from collections import defaultdict
-import heapq
-from typing import List
-
-
 class Twitter:
 
     def __init__(self):
         self.q = defaultdict(list)  # userId 별로 게시물 (max heap)
         self.following = defaultdict(set)  # userId 별로 follow 하고 있는 user
-        self.index = 0
+        # 게시물별 id (가장 최신 Tweets -> 가장 오래된 Tweets 로 가져오기 위함)
+        self.idx = 0
+        self.tweetsCount = 10  # 가져올 트윗 개수
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        heapq.heappush(self.q[userId], (self.index, tweetId))
-        self.index += 1
+        heapq.heappush(self.q[userId], (self.idx, tweetId))
+        self.idx += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        posts = []
+        temp = []
+        temp += heapq.nlargest(self.tweetsCount, self.q[userId])  # 내꺼 가져오기
 
-        posts += heapq.nlargest(10, self.q[userId])  # 내꺼 10개 추가
+        # 내 팔로잉중에 상위 10개씩 가져오기
+        for followeeId in self.following[userId]:
+            temp += heapq.nlargest(self.tweetsCount, self.q[followeeId])
 
-        for id in self.following[userId]:  # 내 팔로잉중에
-            posts += heapq.nlargest(10, self.q[id])
-
+        # 내 상위 10개 게시물 + 내 팔로잉의 상위 10개 게시물 중에 10개 가져오기
         result = []
-        for p in heapq.nlargest(10, posts):
+        for p in heapq.nlargest(self.tweetsCount, temp):
             result.append(p[1])
+
         return result
 
     def follow(self, followerId: int, followeeId: int) -> None:
