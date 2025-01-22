a91cd3b77c50e4c1a165b18262d7dee08a3a12c2
sh kim <kim03208@naver.com>
2025-01-22 23:33:31 +0900

    Time: 59 ms (75.15%), Space: 19.2 MB (50.66%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,36 +1,35 @@
-from collections import defaultdict
-import heapq
-from typing import List
-
-
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        dic = defaultdict(int)
+        interval = defaultdict(int)
 
-        for t in tasks:  # 알파벳 별로 개수 넣고
-            dic[t] += 1
+        # 각 Task 별로 개수 저장
+        for t in tasks:
+            interval[t] += 1
 
-        q = [-d for d in dic.values()]  # 알파벳별 개수를 -로 바꿔서 (max heap)
-        heapq.heapify(q)  # heapq 로 만들기
+        # 알파벳별로 주기 저장
+        q = [-d for d in interval.values()]
+        heapq.heapify(q)  # heapq로
 
-        time = 0
+        result = 0
         while q:
-            cycle = n + 1  # 한 사이클 제한
-            stores = []
+            cycle = n + 1  # 사이클 수
+            stores = []  # 한 사이클에 한번 처리한건 여기 임시로 넣어둠
             task = 0  # 한 사이클에 처리한 일감
 
             while cycle > 0 and q:
-                current = -heapq.heappop(q)  # 하나 꺼내서
-                if current > 1:  # 다음에 또 처리해야하는거면 stores에 임시로 넣어두고
-                    stores.append(-(current - 1))
-                cycle -= 1
-                task += 1
+                curr = heapq.heappop(q)  # 하나 꺼내고
+                cycle -= 1  # 사이클 숫자 줄이고
+                task += 1  # task 1 늘리고
+                if curr < -1:  # 숫자 남아있으면 현재숫자 +1 하고 다시 집어 넣기
+                    stores.append(curr + 1)
 
-            for num in stores:  # 다음에 처리해야하는 task들 q에 넣기
+            # 남은 일감들 q에 넣고
+            for num in stores:
                 heapq.heappush(q, num)
 
-            if not q:  # 다음에 처리할 task 가 없는 경우 -> 끝난거니깐 이번에 처리한 task 만큼 시간 증가
-                time += task
-            else: # 다음에 처리할 task 가 있는 경우 -> 아직 안끝난거니깐 사이클만큼 시간 증가
-                time += n + 1
-        return time
+            if not q:  # 일감없으면 처리한 task만큼 증가
+                result += task
+            else:  # 일감 남아있으면 한 사이클 다 돈거니깐 n+1 만큼 증가
+                result += n + 1
+
+        return result
