69d8796feafc762e0b0b9793caa987210c50c8d4
sh kim <kim03208@naver.com>
2025-02-22 04:01:01 +0900

    Time: 693 ms (86.81%), Space: 18.3 MB (57%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,12 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        INF = float("inf")  # dp[amount]의 최소값을 구해야하기 때문에 inf로 초기 세팅
-        dp = [INF] * (amount + 1)
+        dp = [math.inf] * (amount + 1)  # dp 초기화
         dp[0] = 0
 
         coins.sort()
+
         for c in coins:
             for a in range(c, amount + 1):
                 dp[a] = min(dp[a], dp[a - c] + 1)
-        return dp[amount] if dp[amount] != INF else -1
+
+        return -1 if dp[amount] == math.inf else dp[amount]
