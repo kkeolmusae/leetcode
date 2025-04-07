a644d24b1183b934deb924565b209c0957850252
sh kim <kim03208@naver.com>
2025-04-07 23:13:14 +0900

    Time: 10 ms (97.74%), Space: 37.6 MB (23.71%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,16 +1,13 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        first_num = math.inf
-        second_num = math.inf
-
-        for num in nums:
-            if num <= first_num:  # first_num 배치하고
-                first_num = num
-            elif num <= second_num:
-                # first_num 보다 크고 second_num보다 작으면 second_num 배치
-                second_num = num
+        first = math.inf
+        second = math.inf
+        for curr in nums:
+            if curr <= first:
+                first = curr
+            elif curr <= second:
+                second = curr
             else:
-                # first_num이랑 second_num 보다 크면 조건 중족시킨거니깐
                 return True
 
         return False
