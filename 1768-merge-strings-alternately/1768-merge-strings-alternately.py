154d84b3053499d1fd07fa2934af2cf24eca42e6
sh kim <kim03208@naver.com>
2025-04-07 22:21:00 +0900

    Time: 29 ms (95.58%), Space: 17.7 MB (48.85%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,17 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        w1_idx = 0
+        w2_idx = 0
+        w1_len = len(word1)
+        w2_len = len(word2)
+
         result = ""
-        i, j = 0, 0
+        while w1_idx < w1_len and w2_idx < w2_len:
+            result += word1[w1_idx]
+            result += word2[w2_idx]
+            w1_idx += 1
+            w2_idx += 1
 
-        while i < len(word1) and j < len(word2):
-            result += word1[i] + word2[j]
-            i += 1
-            j += 1
-
-        if i < len(word1):  # w1을 덜 처리한 경우
-            return result + word1[i:]
-
-        if j < len(word2):
-            return result + word2[j:]
+        if w1_idx < w1_len:
+            result += word1[w1_idx:]
+        elif w2_idx < w2_len:
+            result += word2[w2_idx:]
 
         return result
